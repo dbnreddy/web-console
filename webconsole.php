5914488724dd77222bea72948068b155dac7a52f
Bala Nagi Reddy Desam <53612654+dbnreddy@users.noreply.github.com>
2020-01-01 09:44:43 +0530

    Update webconsole.php

--- webconsole.php
+++ webconsole.php
@@ -11,8 +11,8 @@ $NO_LOGIN = false;
 
 // Single-user credentials
 // Example: $USER = 'user'; $PASSWORD = 'password';
-$USER = 'dbnreddy@gmail.com';
-$PASSWORD = '1972Bnr$';
+$USER = 'dbnreddy';
+$PASSWORD = '1972dbnr';
 
 // Multi-user credentials
 // Example: $ACCOUNTS = array('user1' => 'password1', 'user2' => 'password2');
